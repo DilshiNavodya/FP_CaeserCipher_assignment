127dd050b8d174c2a5e42fd7cc38fc1d1ad4f984
Dilshi Navodya <62331157+DilshiNavodya@users.noreply.github.com>
2021-08-09 02:03:00 +0530

    Initial commit

---
+++ caesar_cipher.scala
@@ -0,0 +1,28 @@
+object CeasarCipher{
+
+  val alphabet_U = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  val alphabet_L = "abcdefghijklmnopqrstuvwxyz"
+
+  def encrypt(key: Int, text: String)=text.map {
+    case c if alphabet_U.contains(c)=> rot(alphabet_U,c,key)
+    case c if alphabet_L.contains(c)=> rot(alphabet_L,c,key)
+    case c => c
+  }
+
+  def decrypt(key:Int, text:String)= encrypt(-key,text)
+
+  private def rot(i:IndexedSeq[Char], c:Char, key:Int): Char ={
+    return i((c-i.head+key+i.size)%i.size)
+  }
+}
+object caeser{
+  def main(args: Array[String]){
+    val text="Functional Programming"
+    println("Plaintext  => " + text)
+    println("Encrypted => " + CeasarCipher.encrypt(3,text))
+    println("-------------------------------------------------------")
+    val decry="Ixqfwlrqdo Surjudpplqj"
+    println("Encrypted text  => " + decry)
+    println("Decrypted  => " + CeasarCipher.decrypt(3,decry))
+  }
+}
